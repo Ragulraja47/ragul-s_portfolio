5a87a1a75e0f0246343396b01c59b707d253a538
Ragulraja47 <cragulraja2004@gmail.com>
2025-05-16 20:26:07 +0530

    education content motion completed

--- ragul/src/Components/Education.jsx
+++ ragul/src/Components/Education.jsx
@@ -1,18 +1,29 @@
 import { EXPERIENCES } from "../constants";
+import { motion } from "framer-motion";
 function Education(){
     return(
         <div className="pb-4">
-                    <h2 className="my-20 text-center text-4xl">Experience</h2> 
+                    <motion.h2
+            whileInView={{opacity:1,y:0}}
+            initial={{opacity:0,y:-100}}
+            transition={{duration:0.5}}className="my-20 text-center text-4xl">Experience</motion.h2> 
                     <div>
                         {EXPERIENCES.map( (exp,index) => (
                             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                                <div className="w-full lg:w-1/4">
+                                <motion.div
+                                whileInView={{opacity:1,x:0}}
+                                initial={{opacity:0,x:-100}}
+                                transition={{duration:0.5}}
+                                className="w-full lg:w-1/4">
                                     <p className="mb-2 text-sm text-stone-400">
                                         {exp.year}
                                     </p>
-                                </div>
+                                </motion.div>
         
-                                <div className="w-full max-w-xl lg:w-3/4">
+                                <motion.div
+                                whileInView={{opacity:1,x:0}}
+                                initial={{opacity:0,x:-100}}
+                                transition={{duration:0.5}} className="w-full max-w-xl lg:w-3/4">
                                 <h3 className="mb-2 font-semibold">{exp.role} -{" "}
 
                                 <span className="text-sm text-stone-500">
@@ -23,7 +34,7 @@ function Education(){
                                <p className="mb-4 text-stone-400 ">
                                 {exp.description}
                                </p>
-                                </div>
+                                </motion.div>
                             </div>
                         ))}
                     </div>           
