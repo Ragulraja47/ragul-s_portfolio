e120a3e091766d744f041052e228878ded32d301
Ragulraja47 <cragulraja2004@gmail.com>
2025-05-16 16:24:05 +0530

    hero content motion completed

--- ragul/src/Components/Hero.jsx
+++ ragul/src/Components/Hero.jsx
@@ -1,5 +1,34 @@
 import { HERO_CONTENT } from "../constants";
 import  profilePic  from "../assets/ragul.webp";
+import {motion, stagger} from "framer-motion";
+
+const containerVarients = {
+    hidden:{
+        opacity:0,
+        x:-100
+    },visible:{
+        opacity:1,
+        x:0,
+        transition:{
+            duration:0.5,
+            staggerChildren:0.5,
+        }
+    }
+}
+
+const childVarients = {
+    hidden:{
+        opacity:0,
+        x:-100,
+    } 
+    ,visible:{
+        opacity:1,
+        x:0,
+        transition:{
+            duration:0.5,
+        }
+    } 
+}
 
 function Hero() {
     return (
@@ -7,12 +36,20 @@ function Hero() {
             <div className="flex flex-wrap lg:flex-row-reverse">
                 <div className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:p-16">
-                        <img src={profilePic} alt="Profile image" className="border border-stone-900 rounded-3xl" />
+                        <motion.img src={profilePic} alt="Profile image" className="border border-stone-900 rounded-3xl"
+                        width={650} height={300}
+                        initial={{x:100, opacity:0}}
+                        animate={{x:0,opacity:1}}
+                        transition={{duration:1,delay:1.5}} />
                     </div>
                 </div>
 
-                <div className="w-full lg:w-1/2">
-                    <div className="flex flex-col items-center lg:items-start mt-20">
+                <div               
+                 className="w-full lg:w-1/2">
+                    <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={containerVarients} className="flex flex-col items-center lg:items-start mt-20">
                         <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">Ragul Raja</h2>
                         <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</span>
                         <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
@@ -24,7 +61,7 @@ function Hero() {
                         className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
                             Download Resume
                         </a>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
