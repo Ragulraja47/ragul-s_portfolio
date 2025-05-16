08fe9f9b999a81502810c902f9b134868f382fa1
Ragulraja47 <cragulraja2004@gmail.com>
2025-05-16 19:55:54 +0530

    projects content motion completed

--- ragul/src/Components/Projects.jsx
+++ ragul/src/Components/Projects.jsx
@@ -1,8 +1,12 @@
 import {PROJECTS} from "../constants"
+import { motion } from "framer-motion";
 function Projects(){
     return(
         <div className="pb-4">
-            <h2 className="my-20 text-center text-4xl">Projects</h2> 
+            <motion.h2
+            whileInView={{opacity:1,y:0}}
+            initial={{opacity:0,y:-100}}
+            transition={{duration:1}} className="my-20 text-center text-4xl">Projects</motion.h2> 
             <div>
                 {PROJECTS.map( (project,index) => (
                     <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
@@ -11,7 +15,11 @@ function Projects(){
                             className="mb-6 rounded" />
                         </div> */}
 
-                        <div className="w-full max-w-xl lg:w-3/4">
+                        <motion.div
+                        whileInView={{opacity:1,x:0}}
+                        initial={{opacity:0,x:-100}}
+                        transition={{duration:1}} 
+                        className="w-full max-w-xl lg:w-3/4">
                         <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
 
                         <p className="mb-4 text-stone-400">{project.description}</p>
@@ -21,7 +29,7 @@ function Projects(){
                             key={index}> {tech} </span>
                         ))}
                             
-                        </div>
+                        </motion.div>
                     </div>
                 ))}
             </div>           
