2651de12cdf6cbdff14b0ba5095d601860bdbbbf
Ragulraja47 <cragulraja2004@gmail.com>
2025-05-16 19:49:44 +0530

    tools content motion completed

--- ragul/src/Components/Education.jsx
+++ ragul/src/Components/Education.jsx
@@ -2,7 +2,7 @@ import { EXPERIENCES } from "../constants";
 function Education(){
     return(
         <div className="pb-4">
-                    <h2 className="my-20 text-center text-4xl">Education</h2> 
+                    <h2 className="my-20 text-center text-4xl">Experience</h2> 
                     <div>
                         {EXPERIENCES.map( (exp,index) => (
                             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
--- ragul/src/Components/Tools.jsx
+++ ragul/src/Components/Tools.jsx
@@ -3,29 +3,77 @@ import {
   SiKubernetes,
   SiGithub,
   SiXampp,
+  SiPostman,
 } from "react-icons/si";
+import { motion } from "framer-motion";
+
+  const iconVariants = (duration)=>({
+    initial:{y:-10},
+    animate:{
+        y:[10,-10],
+        transition:{
+            duration:duration,
+            ease:"linear",
+            repeat:Infinity,
+            repeatType:"reverse",
+        }
+    }
+  })
+
+
 function Tools() {
     return (
         <div className="pb-24">
-            <h2 className="my-20 text-center text-4xl">Tools</h2>
-            <div className="flex flex-wrap items-center justify-center gap-8">
-                <div>
+             <motion.h2
+            whileInView={{opacity:1,y:0}}
+            initial={{opacity:0,y:-100}}
+            transition={{duration:1.5}} className="my-20 text-center text-4xl">Tools</motion.h2>
+            <motion.div
+             whileInView={{opacity:1,x:0}}
+            initial={{opacity:0,x:-100}}
+            transition={{duration:1.5}} 
+             className="flex flex-wrap items-center justify-center gap-8">
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2)}
+                >
                       <SiDocker className="text-blue-500 text-7xl" />  
-                </div>
+                </motion.div>
 
-                <div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(5)}
+                >
                     <SiKubernetes className="text-blue-600 text-7xl" /> 
-                </div>
+                </motion.div>
 
-                <div>
+                <motion.div
+                                initial="initial"
+                                animate="animate"
+                                variants={iconVariants(7)}
+                                >
                     <SiGithub className="text-gray-800 text-7xl" />  
-                </div>
+                </motion.div>
 
-                <div>
+                <motion.div
+                         initial="initial"
+                         animate="animate"
+                         variants={iconVariants(5)}
+                           >
                     <SiXampp className="text-orange-500 text-7xl" /> 
-                </div>
+                </motion.div>
 
-            </div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2)}
+                >
+                <SiPostman className="text-orange-500 text-7xl" />
+                </motion.div>
+
+            </motion.div>
         </div>
     )
 }
